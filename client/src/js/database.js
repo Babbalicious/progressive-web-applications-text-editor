fc80d5efc2dfccc09a7f0dab80b64a47d0ec33ed
Chris Babb <Chris Babb>
2024-06-25 21:18:28 -0500

    updated database and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jate_Db = await openDB('jate', 1);
+  const tx = jate_Db.transaction('jate', 'readwrite'); // changed 'todo' to 'todos'
+  const store = tx.objectStore('jate');
+  const request = store.put(content);
+  const result = await request;
+  console.log('Data saved to the database', result);
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  const jate_Db = await openDB('contact', 1);
+  const tx = jate_Db.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
